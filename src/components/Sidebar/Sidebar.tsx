077da5ca13d769773e72c029528e776a33bc9e12
sClarkeDev <sclarkedev@gmail.com>
2024-05-13 10:33:53 +0100

    Sidebar Titles

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -2,18 +2,24 @@
 
 import { useCrimes } from '@/context/Crimes';
 import { CrimeList } from '../CrimeList';
+import { Separator } from '../ui/separator';
 
 export const Sidebar = () => {
   const { crimes } = useCrimes();
 
   return (
-    <div className="flex-1 flex flex-col animate-in slide-in-from-right py-8">
+    <div className="flex-1 flex animate-in slide-in-from-right py-8">
       {crimes.length ? (
-        <>
+        <div className="flex-1 flex flex-col space-y-8">
+          <div className="px-8 space-y-2">
+            <h2 className="text-3xl font-semibold tracking-tight">West Midlands Police</h2>
+            <p className="text-lg text-muted-foreground">London</p>
+          </div>
+          <Separator />
           <div className="flex-1 overflow-y-auto">
             <CrimeList />
           </div>
-        </>
+        </div>
       ) : (
         <div className="flex-1 flex items-center justify-center">
           <p>No results.</p>
